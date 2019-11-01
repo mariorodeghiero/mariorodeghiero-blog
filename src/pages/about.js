000986bf857009399894b5cbc71eacc4f55c45b3
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2019-10-31 21:13:15 -0300

    add: links

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const AbautPage = () => (
+    <h1>About Page</h1>
+)
+
+export default AbautPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,7 +7,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
-
+    <ul>
+      <li>
+        <Link to="/about" activeStyle={{ color: "blue" }}>About</Link>
+      </li>
+      <li>
+        <Link to="/home" activeStyle={{ color: "blue" }}>Home</Link>
+      </li>
+    </ul>
   </Layout>
 )
 

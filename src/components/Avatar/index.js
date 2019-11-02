21295dee000786e5ede9d6ffb2e57f7192ad5510
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2019-11-02 12:42:40 -0300

    add: gatsby-image

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,7 +1,20 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const Avatar = () => (
-    <h1>About Page</h1>
-)
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(graphql`
+    query {
+      avatarImage: file(relativePath: { eq: "perfil.jpeg" }) {
+        childImageSharp {
+          fixed(width: 90, height: 90) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return <Img fixed={avatarImage.childImageSharp.fixed} fadeIn />
+}
 
 export default Avatar

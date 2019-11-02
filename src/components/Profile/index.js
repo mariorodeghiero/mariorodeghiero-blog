226a28a942951b877ed53d888f492d876da749ef
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2019-11-01 23:25:03 -0300

    add: graphql with useStaticQuery

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,31 +1,29 @@
 import React from "react"
-import { StaticQuery, graphql} from 'gatsby'
+import { useStaticQuery, graphql } from "gatsby"
 
-const Profile = () => (
-    <StaticQuery 
-        query={graphql`
-            query MySiteMetadata {
-                site {
-                    siteMetadata {
-                        title
-                        position
-                        description
-                    }
-                }
-            }
-        `}
-        render={({
-            site: { 
-                siteMetadata: { title, position, description},
-            },
-        }) => (
-            <div>
-                <h1>{title}</h1>
-                <h2>{position}</h2>
-                <p>{description}</p>
-            </div>
-        )}
-    />
-)
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default Profile

05c7e8b1a8d9c29187c893ac8fae38afc5642522
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2019-11-02 13:44:35 -0300

    add: social icons components

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+const handle = "mariorodeghiero"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/${handle}`,
+  },
+]
+
+export default links

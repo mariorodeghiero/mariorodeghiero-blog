e5f218736864d6fcb20622d3e98bfed44a2f17be
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2019-11-05 22:47:50 -0300

    add: post style

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import * as S from "../components/Post/styled"
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
@@ -10,8 +11,17 @@ const BlogPost = ({ data }) => {
   return (
     <>
       <Layout>
-        <h1>Title: {post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+        <SEO title={post.frontmatter.title} />
+        <S.PostHeader>
+          <S.PostDate>
+            {post.frontmatter.date} • {post.timeToRead} min to read
+          </S.PostDate>
+          <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+          <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+        </S.PostHeader>
+        <S.MainContent>
+          <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+        </S.MainContent>
       </Layout>
     </>
   )
@@ -29,6 +39,7 @@ export const query = graphql`
         date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `

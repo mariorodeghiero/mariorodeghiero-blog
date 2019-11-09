dafa8a9fb06e5e733ee4a77914fd616167c07cec
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2019-11-08 22:01:23 -0300

    add: disqus commnents

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import RecommendedPosts from "../components/RecommendedPosts"
+import Comments from "../components/Comments"
 import SEO from "../components/seo"
 import * as S from "../components/Post/styled"
 
@@ -26,6 +27,7 @@ const BlogPost = ({ data, pageContext }) => {
           <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
         </S.MainContent>
         <RecommendedPosts next={next} previous={previous} />
+        <Comments url={post.fields.slug} title={post.frontmatter.title} />
       </Layout>
     </>
   )

4bde8292fc52525278b8544181969bfc4a1f544c
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-04-28 21:20:27 -0300

    [add][s]: add about template - blog #2

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Resume from "../templates/resume"
 
 const AbautPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>Mário Antônio do Amaral Rodeghiero</h1>
+    <Resume/>
   </Layout>
 )
 
